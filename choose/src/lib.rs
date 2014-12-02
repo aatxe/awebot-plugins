fc261f4f5cebd2b9411c5fe93b06b9bbc233a37d
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-02 18:07:42 -0500

    Added a plugin to choose randomly between options separated by "or".

---
+++ choose/src/lib.rs
@@ -0,0 +1,34 @@
+#![feature(if_let, slicing_syntax)]
+extern crate irc;
+
+use std::io::{BufferedReader, BufferedWriter, IoResult};
+use std::rand::{task_rng, sample};
+use irc::conn::NetStream;
+use irc::data::Message;
+use irc::data::kinds::{IrcReader, IrcWriter};
+use irc::server::utils::Wrapper;
+
+#[no_mangle]
+pub fn process<'a>(server: &'a Wrapper<'a, BufferedReader<NetStream>, BufferedWriter<NetStream>>, 
+                   message: Message) -> IoResult<()> {
+    let mut args = Vec::new();
+    let msg_args: Vec<_> = message.args.iter().map(|s| s[]).collect();
+    args.push_all(msg_args[]);
+    if let Some(ref suffix) = message.suffix {
+        args.push(suffix[])
+    }
+    let source = message.prefix.unwrap_or(String::new());
+    process_internal(server, source[], message.command[], args[])
+}
+
+pub fn process_internal<'a, T, U>(server: &'a Wrapper<'a, T, U>, source: &str, command: &str,
+                               args: &[&str]) -> IoResult<()> where T: IrcReader, U: IrcWriter {
+    let user = source.find('!').map_or("", |i| source[..i]);
+    if let ("PRIVMSG", [chan, msg]) = (command, args) {
+        if msg.starts_with("@choose ") {            
+            let res = sample(&mut task_rng(), msg[8..].split_str(" or "), 1);
+            try!(server.send_privmsg(chan, format!("{}: {}", user, res[0])[]));
+        }
+    }
+    Ok(())
+}
