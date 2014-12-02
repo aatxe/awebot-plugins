fba9e4423da5dc626f9ca646191a3665e2134eb5
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-02 18:19:13 -0500

    Added a plugin to speak through the bot via query.

---
+++ puppet/src/lib.rs
@@ -0,0 +1,40 @@
+#![feature(if_let, slicing_syntax)]
+extern crate irc;
+
+use std::io::{BufferedReader, BufferedWriter, IoResult};
+use irc::conn::NetStream;
+use irc::data::Message;
+use irc::data::kinds::{IrcReader, IrcWriter};
+use irc::server::Server;
+use irc::server::utils::Wrapper;
+
+#[no_mangle]
+pub fn process<'a>(server: &'a Wrapper<'a, BufferedReader<NetStream>, BufferedWriter<NetStream>>, 
+                   message: Message) -> IoResult<()> {
+    let mut args = Vec::new();
+    let msg_args: Vec<_> = message.args.iter().map(|s| s[]).collect();
+    args.push_all(msg_args[]);
+    if let Some(ref suffix) = message.suffix {
+        args.push(suffix[])
+    }
+    let source = message.prefix.unwrap_or(String::new());
+    process_internal(server, source[], message.command[], args[])
+}
+
+pub fn process_internal<'a, T, U>(server: &'a Wrapper<'a, T, U>, source: &str, command: &str,
+                               args: &[&str]) -> IoResult<()> where T: IrcReader, U: IrcWriter {
+    let user = source.find('!').map_or("", |i| source[..i]);
+    if let ("PRIVMSG", [chan, msg]) = (command, args) {
+        let tokens: Vec<_> = msg.split_str(" ").collect();
+        if chan != server.config().nickname[] { return Ok(()) }
+        if server.config().is_owner(user) {
+            if msg.starts_with("#") || msg.starts_with("$") {
+                if tokens.len() > 1 {
+                    try!(server.send_privmsg(if tokens[0].starts_with("$") { tokens[0][1..] 
+                                             } else { tokens[0] }, msg[tokens[0].len()+1..]));
+                }
+            }
+        }
+    }
+    Ok(())
+}
