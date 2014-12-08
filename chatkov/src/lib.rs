0308843b666c9a1d4dc6eee28e35521cbe4607cc
Aaron Weiss <aaronweiss74@gmail.com>
2014-12-08 03:30:10 -0500

    Added basic text generation using Markov chain.

---
+++ chatkov/src/lib.rs
@@ -0,0 +1,73 @@
+#![feature(slicing_syntax)]
+extern crate irc;
+extern crate markov;
+
+use std::io::{BufferedReader, BufferedWriter, FileAccess, FileMode, IoResult};
+use std::io::fs::File;
+use std::str::replace;
+use irc::conn::NetStream;
+use irc::data::Message;
+use irc::data::kinds::{IrcReader, IrcWriter};
+use irc::server::utils::Wrapper;
+use markov::Chain;
+
+#[no_mangle]
+pub fn process<'a>(server: &'a Wrapper<'a, BufferedReader<NetStream>, BufferedWriter<NetStream>>, 
+                   message: Message) -> IoResult<()> {
+    let mut args = Vec::new();
+    let msg_args: Vec<_> = message.args.iter().map(|s| s[]).collect();
+    args.push_all(msg_args[]);
+    if let Some(ref suffix) = message.suffix {
+        args.push(suffix[])
+    }
+    let source = message.prefix.unwrap_or(String::new());
+    process_internal(server, source[], message.command[], args[])
+}
+
+pub fn process_internal<'a, T, U>(server: &'a Wrapper<'a, T, U>, source: &str, command: &str,
+                               args: &[&str]) -> IoResult<()> where T: IrcReader, U: IrcWriter {
+    let user = source.find('!').map_or("", |i| source[..i]);
+    if let ("PRIVMSG", [chan, msg]) = (command, args) {
+        let path = Path::new("data/chatkov");
+        if !msg.starts_with("@") {
+            let mut file = File::open_mode(&path, FileMode::Append, FileAccess::Write);
+            try!(file.write_line(replace(msg, ".", "\n")[]));
+        } else if msg.starts_with("@markov") {
+            let mut chain = Chain::new("START".into_string(), "END".into_string());
+            chain.feed_file(&path);
+            try!(server.send_privmsg(chan, format!("{}: {}", user, chain.generate_str())[]));
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::default::Default;
+    use std::io::{MemReader, MemWriter};
+    use irc::conn::Connection;
+    use irc::server::{IrcServer, Server};
+    use irc::server::utils::Wrapper;
+
+    fn test_helper(input: &str) -> String {
+        let server = IrcServer::from_connection(Default::default(), Connection::new(
+            MemReader::new(input.as_bytes().to_vec()), MemWriter::new()
+        ));
+        for message in server.iter() {
+            println!("{}", message);
+            let mut args = Vec::new();
+            let msg_args: Vec<_> = message.args.iter().map(|s| s[]).collect();
+            args.push_all(msg_args[]);
+            if let Some(ref suffix) = message.suffix {
+                args.push(suffix[])
+            }
+            let source = message.prefix.unwrap_or(String::new());
+            super::process_internal(
+                &Wrapper::new(&server), source[], message.command[], args[]
+            ).unwrap();
+        }
+        String::from_utf8(server.conn().writer().get_ref().to_vec()).unwrap()
+    }
+    
+    // TODO: add tests
+}
