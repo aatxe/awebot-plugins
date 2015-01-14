9a2b7fa37be1f347d8e8b7dccee7bfa63e701a40
Aaron Weiss <aaronweiss74@gmail.com>
2015-01-14 01:29:39 -0500

    Added test for lewd.

--- lewd/src/lib.rs
+++ lewd/src/lib.rs
@@ -16,7 +16,7 @@ static MESSAGES: &'static [&'static str] =
  , "You've got 206 bones in your body. Want one more?"
  , "Your daddy must have been a baker because you've got a nice set of buns."
  , "If I told you you had a beautiful chest, would you hold it against me?"
- , "Wan to play army? I'll lay down, and you can blow the hell out of me."
+ , "Want to play army? I'll lay down, and you can blow the hell out of me."
  , "That shirt is very becoming on you.  If I were on you, I'd be coming too."
  , "You might not be the best looking girl here, but beauty is only a light switch away."
  , "Nice shoes. Want to screw?"
@@ -24,7 +24,7 @@ static MESSAGES: &'static [&'static str] =
  , "Something tells me you're sweet. Can I have a taste?"
  , "Do you work for UPS? Because I swear I saw you checking out my package."
  , "Do you want to come over to my place and feed your beaver some wood?"
- , "Want to play Kite? I lay down, you blow, and we’ll see how high you can take me."
+ , "Want to play Kite? I lay down, you blow, and we'll see how high you can take me."
  , "That outfit would look great in a crumpled heap on my bedroom floor tomorrow morning."
  ];
 
@@ -48,6 +48,7 @@ pub fn process_internal<'a, T, U>(server: &'a Wrapper<'a, T, U>, msg: &Message) 
 
 #[cfg(test)]
 mod test {
+    use std::borrow::ToOwned;
     use std::default::Default;
     use std::io::{MemReader, MemWriter};
     use irc::client::conn::Connection;
@@ -65,6 +66,11 @@ mod test {
         }
         String::from_utf8(server.conn().writer().get_ref().to_vec()).unwrap()
     }
-    
-    // TODO: add tests
+
+    #[test]
+    fn lewd() {
+        let data = test_helper(":test!test@test PRIVMSG #test :test\r\n");
+        assert!(super::MESSAGES.iter().map(|s| format!("PRIVMSG test :{}\r\n", s))
+                .collect::<Vec<_>>().contains(&data.to_owned()) || &data[] == "");
+    }
 }
