6e1b2b7d4fb4238d8738fd344266b36afae5ad13
Aaron Weiss <aaronweiss74@gmail.com>
2015-01-14 00:37:03 -0500

    Added plugin to randomly send lewd messages to people.

---
+++ lewd/src/lib.rs
@@ -0,0 +1,70 @@
+#![allow(unstable)]
+#![feature(slicing_syntax)]
+extern crate irc;
+
+use std::io::{BufferedReader, BufferedWriter, IoResult};
+use std::rand::{thread_rng, Rng};
+use irc::client::conn::NetStream;
+use irc::client::data::{Command, Message};
+use irc::client::data::Command::PRIVMSG;
+use irc::client::data::kinds::{IrcReader, IrcWriter};
+use irc::client::server::utils::Wrapper;
+
+static MESSAGES: &'static [&'static str] = 
+&[ "Hey, baby. Want some fuck?"
+ , "If I were you, I'd have sex with me."
+ , "You've got 206 bones in your body. Want one more?"
+ , "Your daddy must have been a baker because you've got a nice set of buns."
+ , "If I told you you had a beautiful chest, would you hold it against me?"
+ , "Wan to play army? I'll lay down, and you can blow the hell out of me."
+ , "That shirt is very becoming on you.  If I were on you, I'd be coming too."
+ , "You might not be the best looking girl here, but beauty is only a light switch away."
+ , "Nice shoes. Want to screw?"
+ , "I'd like to kiss you passionately on the lips. Then, I'll move up to your belly button."
+ , "Something tells me you're sweet. Can I have a taste?"
+ , "Do you work for UPS? Because I swear I saw you checking out my package."
+ , "Do you want to come over to my place and feed your beaver some wood?"
+ , "Want to play Kite? I lay down, you blow, and we’ll see how high you can take me."
+ , "That outfit would look great in a crumpled heap on my bedroom floor tomorrow morning."
+ ];
+
+#[no_mangle]
+pub fn process<'a>(server: &'a Wrapper<'a, BufferedReader<NetStream>, BufferedWriter<NetStream>>, 
+                   message: Message) -> IoResult<()> {
+    process_internal(server, &message)
+}
+
+pub fn process_internal<'a, T, U>(server: &'a Wrapper<'a, T, U>, msg: &Message) -> IoResult<()> 
+    where T: IrcReader, U: IrcWriter {
+    let user = msg.get_source_nickname().unwrap_or("");
+    if let Ok(PRIVMSG(_, _)) = Command::from_message(msg) {
+        let mut rng = thread_rng();
+        if rng.gen_weighted_bool(20) {
+            try!(server.send_privmsg(user, *rng.choose(MESSAGES).unwrap()));
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use std::default::Default;
+    use std::io::{MemReader, MemWriter};
+    use irc::client::conn::Connection;
+    use irc::client::server::{IrcServer, Server};
+    use irc::client::server::utils::Wrapper;
+
+    fn test_helper(input: &str) -> String {
+        let server = IrcServer::from_connection(Default::default(), Connection::new(
+            MemReader::new(input.as_bytes().to_vec()), MemWriter::new()
+        ));
+        for message in server.iter() {
+            let message = message.unwrap();
+            println!("{:?}", message);
+            super::process_internal(&Wrapper::new(&server), &message).unwrap();
+        }
+        String::from_utf8(server.conn().writer().get_ref().to_vec()).unwrap()
+    }
+    
+    // TODO: add tests
+}
